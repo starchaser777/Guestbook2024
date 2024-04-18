c77c2e9eaeaefd2508023e94e507c9d0ff696821
starchaser777 <les980730@gmail.com>
2024-04-18 11:45:55 +0900

    Initial commit

---
+++ src/main/java/kr/ac/kopo/guestbook2024/Guestbook2024Application.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.guestbook2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guestbook2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Guestbook2024Application.class, args);
+    }
+
+}
---
+++ src/main/java/kr/ac/kopo/guestbook2024/controller/GuestbookController.java
@@ -0,0 +1,14 @@
+package kr.ac.kopo.guestbook2024.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/guestbook")
+public class GuestbookController {
+    @GetMapping({"/", "/list"})
+    public String list() {
+        return "/guestbook/list";
+    }
+}
